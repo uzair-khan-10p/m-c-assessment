3fcdc95874f1d354d56c5fa8d51c3fe55721498c
uzair-khan-10p <muhammad.uzair@tenpealrs.com>
2020-12-11 16:11:14 +0500

    meetings scheduler logic added

--- meetings_scheduler.rb
+++ meetings_scheduler.rb
@@ -1,6 +1,7 @@
 # This class will schedule your meetings in a 9-5 working hours
 class MeetingsScheduler
   attr_reader :onsite_meetings, :offsite_meetings
+  attr_accessor :start_time
 
   HOURS_SPAN = 8
 
@@ -9,6 +10,8 @@ class MeetingsScheduler
 
     @onsite_meetings = meetings.select { |m| m[:type] == :onsite }
     @offsite_meetings = meetings.select { |m| m[:type] == :offsite }
+    @start_time = Time.new(2020, 01, 01, 9, 0, 0) # 9 AM time object initialized
+    @scheduled_meetings = []
   end
 
   def schedule
@@ -17,7 +20,15 @@ class MeetingsScheduler
       return
     end
 
-    scheduled_meetings = []
+    onsite_meetings.each { |meeting| assign_slot meeting }
+
+    offsite_meetings.each do |meeting|
+      @start_time += 1800 # pad 30 minutes for offsite meetings
+      assign_slot meeting
+    end
+
+    puts 'Yes, can fit. One possible solution would be:'
+    @scheduled_meetings.each { |m| puts m  }
   end
 
   def can_fit?
@@ -26,5 +37,17 @@ class MeetingsScheduler
     offsite_meetings.each { |m| total_hours += m[:duration] + 0.5 }
 
     total_hours <= HOURS_SPAN
+  end
+
+  private
+
+  def format(time)
+    time.strftime("%l:%M %p")
+  end
+
+  def assign_slot(meeting)
+    end_time = start_time + ( meeting[:duration] * 3600 )
+    @scheduled_meetings << "#{format(start_time)} - #{format(end_time)} - #{meeting[:name]}"
+    @start_time = end_time
   end
 end
--- test.rb
+++ test.rb
@@ -1,6 +1,13 @@
 require './meetings_scheduler'
 
 # assuming duration in hours
+example_0 = [
+  { name: 'Meeting 1', duration: 3, type: :onsite },
+  { name: 'Meeting 2', duration: 2, type: :offsite },
+  { name: 'Meeting 3', duration: 1, type: :offsite },
+  { name: 'Meeting 4', duration: 0.5, type: :onsite }
+]
+
 example_1 = [
   { name: 'Meeting 1', duration: 1.5, type: :onsite },
   { name: 'Meeting 2', duration: 2, type: :offsite },
@@ -9,5 +16,18 @@ example_1 = [
   { name: 'Meeting 5', duration: 1, type: :offsite },
 ]
 
-scheduler = MeetingsScheduler.new(example_1)
+example_2 = [
+  { name: 'Meeting 1', duration: 4, type: :offsite },
+  { name: 'Meeting 2', duration: 4, type: :offsite }
+]
+
+example_3 = [
+  { name: 'Meeting 1', duration: 0.5, type: :offsite },
+  { name: 'Meeting 2', duration: 0.5, type: :onsite },
+  { name: 'Meeting 3', duration: 2.5, type: :offsite },
+  { name: 'Meeting 4', duration: 3, type: :onsite }
+]
+
+# change the example below to test different datasets.
+scheduler = MeetingsScheduler.new(example_0)
 scheduler.schedule
